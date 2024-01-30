ee585976ac6838c3b8fc54c1e8863fbba3d1e6c1
Alfathere <alfat601@smk.belajar.id>
2024-01-30 13:43:59 +0700

    return

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASE_URL", "http://localhost/jclothesmvc/");
+define("BASE_IMAGE", "http://localhost/jclothesmvc/public/img/");
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "jclothesmvc");
